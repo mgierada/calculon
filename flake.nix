e36adf3d8a511394171bd5f7a01db9664542dc8c
Maciej Gierada <maciej.gierada@sofomo.com>
2026-06-29 10:59:27 +0200

    feat: add nix setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Calculon";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go_1_26
+            pkgs.gitleaks
+            pkgs.go-migrate
+            pkgs.git
+          ];
+        };
+      });
+}
